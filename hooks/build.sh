4ec88b1f8e692a86258b5cf2f7722d3cbd42322b
Sumanth Nirmal <sumanth.724@gmail.com>
2020-04-26 03:14:41 -0700

    Update build hook for github workflow

--- hooks/build.sh
+++ hooks/build.sh
@@ -1,1 +1,13 @@
 echo $1
+echo $GITHUB_SHA
+echo $GITHUB_REF
+pwd
+
+
+docker build \
+   --network host \
+   --label ade_image_commit_sha="$GITHUB_SHA" \
+   --label ade_image_commit_tag="$GITHUB_REF" \
+   -t "$1" \
+   --file 
+
