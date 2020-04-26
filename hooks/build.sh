64b4851a6aefa38e73170195d9f7d6a086e2e44b
Sumanth Nirmal <sumanth.724@gmail.com>
2020-04-26 16:35:43 -0700

    Fix build and upload hooks with image falvours and tags

--- hooks/build.sh
+++ hooks/build.sh
@@ -1,8 +1,20 @@
-# change to the directory where the Dockerfile exists
-cd $1
+img_name=$1
+img_flavour=$2
+# fully qualified name for image
+img_fqn=""
+[[ -z "$img_flavour" ]] && img_fqn=$img_name || img_fqn=$img_name-$img_flavour
+img_tag=$img_fqn
+commit_sha=$GITHUB_SHA
+# extract tag from GITHUB_REF if any
+tag=$(echo $GITHUB_REF | grep -o -E 'tags[^ ]+' | sed 's/tags\///')
+
+echo "Building image "$img_tag
+
+# change to the directory where the Dockerfile exists 
+cd $img_name
 docker build \
    --network host \
-   --label ade_image_commit_sha="$GITHUB_SHA" \
-   --label ade_image_commit_tag="$GITHUB_REF" \
-   -t "$1" .
+   --label ade_image_commit_sha="$commit_sha" \
+   --label ade_image_commit_tag="$tag" \
+   -t "$img_tag":latest .
 
--- hooks/upload.sh
+++ hooks/upload.sh
@@ -1,2 +1,15 @@
-echo $1
-echo $1 secrets.DOCKERHUB_USERNAME
+img_name=$1
+img_flavour=$2
+# fully qualified name for image
+img_fqn=""
+[[ -z "$img_flavour" ]] && img_fqn=$img_name || img_fqn=$img_name-$img_flavour
+img_tag=$img_fqn
+registry='sumanthnirmal/ade-volumes'
+
+echo "Tagging "$img_tag 
+echo "Pushing to "$registry:$img_tag
+
+docker tag $img_tag:latest $registry:$img_tag
+docker push $registry:$img_tag
+
+
